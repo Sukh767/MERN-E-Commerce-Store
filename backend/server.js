3752551800afb5bcf0afe3d680c9925f2b570ee5
Sukh767 <sukharanjanjana767@gmail.com>
2024-10-10 02:23:08 +0530

    setup mongoDB, config file and define Schema for order,product,user

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -23,4 +27,4 @@ app.get('/api/products/:id',(req,res) => {
 
 const PORT = process.env.PORT || 5000
 app.listen(
-  PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+  PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
