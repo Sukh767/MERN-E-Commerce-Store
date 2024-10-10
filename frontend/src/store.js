00b9e2600be07582733bab2cb78792f67f1e83cb
Sukh767 <sukharanjanjana767@gmail.com>
2024-10-10 18:57:52 +0530

    create a Redux Store

---
+++ frontend/src/store.js
@@ -0,0 +1,26 @@
+// src/store.js
+import { configureStore } from "@reduxjs/toolkit";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// Example reducers (you should replace these with your actual reducers)
+
+// Combine your reducers here
+const reducer = {};
+
+// Initial state (can be an empty object or populated as needed)
+const initialState = {};
+
+// Middleware setup (thunk is included by default in configureStore)
+const middleware = [thunk];
+
+// Configure the store
+const store = configureStore({
+  reducer,
+  initialState,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middleware),
+  devTools: composeWithDevTools(), // You can optionally enable devtools
+});
+
+export default store;
