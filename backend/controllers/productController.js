97d6d6ce9750cd12fa32e0b11c717a770d7bc8b0
Sukh767 <sukharanjanjana767@gmail.com>
2024-10-14 00:18:06 +0530

    Cleaning code and Add productController

---
+++ backend/controllers/productController.js
@@ -0,0 +1,39 @@
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+import mongoose from "mongoose";
+
+
+
+// @description Fetch all products
+// @route GET /api/products
+// @access Public
+const getProducts = asyncHandler(async(req,res) => {
+  const products = await Product.find({});
+  res.json(products);
+})
+
+// @description Fetch all products
+// @route GET /api/products/:id
+// @access Public
+
+const getProductById = asyncHandler(async(req,res) => {
+    // Validate if the id is a valid ObjectId
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+})
+
+
+export {
+  getProducts,
+  getProductById,
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,40 +1,11 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-import mongoose from "mongoose";
+import { getProductById, getProducts } from "../controllers/productController.js";
 const router = express.Router();
-import Product from "../models/productModel.js";
 
-// @description Fetch all products
-// @route GET /api/products
-// @access Public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+// @description Fetch all products(pass controller function)
+router.route("/",).get(getProducts);
 
-// @description Fetch single product
-// @route GET /api/products/:id
-// @access Public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    // Validate if the id is a valid ObjectId
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+// @description Fetch single product(pass controller function)
+router.route('/:id').get(getProductById)
 
 export default router;
