77fe05d28318b1df2f088e3420f16329e96a8e92
Sukh767 <sukharanjanjana767@gmail.com>
2024-10-15 01:10:27 +0530

    User Registration and password encrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,6 +25,40 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @description Register a new user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name,email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+
+  if(userExist){
+    res.status(400)
+    throw new Error('User already exist')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if(user){
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  }else{
+    res.status(400)
+    throw new Error('Invalid User data')
+  }
+});
+
+
 // @description user profile
 // @route POST /api/users/profile
 // @access Private
@@ -44,4 +78,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser,getUserProfile };
+export { authUser,registerUser,getUserProfile};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { authUser,getUserProfile } from "../controllers/userController.js";
+import { authUser, registerUser, getUserProfile } from "../controllers/userController.js";
 const router = express.Router();
 import { protect } from "../middleware/authmiddleware.js";
 
+
+
+router.route('/').post(registerUser)
 // @description login (pass controller function)
 router.post("/login",authUser)
 router.route('/profile').get(protect, getUserProfile)
