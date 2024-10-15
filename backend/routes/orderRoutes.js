cd0be283d8869630a5c9e118c76ac1e28fd20c99
Sukh767 <sukharanjanjana767@gmail.com>
2024-10-16 03:45:25 +0530

    Implement Get order By Id Endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems } from "../controllers/orderController.js";
+import { addOrderItems, getOrderById } from "../controllers/orderController.js";
 import { protect } from "../middleware/authmiddleware.js";
 
 
 
 router.route('/').post(protect,addOrderItems)
+router.route('/:id').get(protect,getOrderById)
 
 
 
