f208646af5345db043f023de7fd71dc00f3debbc
Sukh767 <sukharanjanjana767@gmail.com>
2024-10-16 05:57:45 +0530

    implement Update To Paid Endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -52,6 +52,32 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
+
+// @description      Update order to paid
+// @route            GET/api/orders/:id/pay
+// @access           Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+  if(order){
+    order.isPaid = true,
+    order.paidAt = Date.now()
+    //PayPal response
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address
+    }
+
+    const updateOrder = await order.save()
+
+    res.json(updateOrder)
+  }else{
+    res.status(404)
+    throw new Error('Order not found')
+  }
+});
+
 export {
-  addOrderItems,getOrderById
+  addOrderItems,getOrderById,updateOrderToPaid
 }
