07b73bcaa64f2f87683624fe6ba57ff285e4b3ff
Sukh767 <sukharanjanjana767@gmail.com>
2024-10-17 13:00:50 +0530

    Fix issue on Admin screen access security

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -5,17 +5,26 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { listUsers } from '../actions/userActions';
+import { useNavigate } from 'react-router-dom';
 
 const UserListScreen = () => {
 
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const userList = useSelector(state => state.userList)
   const {loading, error, users} = userList
 
+  const userLogin = useSelector(state => state.userLogin)
+  const {userInfo} = userLogin
+
   useEffect(()=>{
-    dispatch(listUsers())
-  },[dispatch])
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers())
+    } else {
+      navigate('/login')
+    }
+  },[dispatch,navigate,userInfo])
 
   const deleteHandler = (id) =>{
     console.log('delete')
