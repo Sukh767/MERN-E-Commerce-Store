2fb110449d77ce2c866f1990aed80732b56d1776
Sukh767 <sukharanjanjana767@gmail.com>
2024-10-17 14:53:32 +0530

    Changing Users details and import to database

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,18 +4,18 @@ const users = [
   {
     name: "Lucifer",
     email: "admin@example.com",
-    password: bcrypt.hashSync("123456", 10),
+    password: bcrypt.hashSync("lucifer1", 12),
     isAdmin: true,
   },
   {
     name: "Tarun Kumar",
     email: "tarunlkr6@gmail.com",
-    password: bcrypt.hashSync("123456", 10),
+    password: bcrypt.hashSync("12345", 12),
   },
   {
-    name: "Prem Jana",
-    email: "prem@gmail.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: "Tony Stark",
+    email: "tony@gmail.com",
+    password: bcrypt.hashSync("12345", 12),
   },
 ];
 
