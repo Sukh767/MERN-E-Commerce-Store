c7e0599ad2c032e67d413f434bcc3a36b3ed1d81
Sukh767 <sukharanjanjana767@gmail.com>
2024-10-19 05:50:02 +0530

    Complete setup, For deployment

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ import morgan from 'morgan';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
-import uploadRoutes from './routes/uploadRoutes.js'
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 
@@ -16,28 +16,35 @@ connectDB();
 
 const app = express();
 
-if(process.env.NODE_ENV === 'development'){
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 //body-parser-accept json
-app.use(express.json())
-
-app.get('/', (req, res) => {
-  res.send('API Is Working...Welcome to ProShop.');
-});
+app.use(express.json());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/orders',orderRoutes);
-app.use('/api/upload',uploadRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
-app.get('/api/config/paypal',(req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID)
-})
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
 
-const __dirname = path.resolve()
-app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API Is Working...Welcome to ProShop.');
+  });
+}
 
 app.use(notFound);
 
